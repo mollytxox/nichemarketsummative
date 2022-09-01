e0b250a17bac14e5413fab170b098a75e9fb8bb5
mollytxox <270019773@yoobeestudent.ac.nz>
2022-09-01 18:37:02 +1200

    connected project to mongodb and displayed some test products

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // Setting up our dependencies
 const express = require("express");
 const app = express();
-const port = 3100;
+const port = 3400;
 const cors = require("cors");
 // passes information from the frontend to the backend
 const bodyParser = require("body-parser");
@@ -15,9 +15,7 @@ console.log(config)
 
 // Schemas
 // every schema needs a capital letter
-// const Coffee = require("./models/coffee.js");
-// const User = require("./models/user.js");
-// const Review = require("./models/review.js");
+const Product = require("./models/product.js");
 
 // start our dependencies
 app.use(bodyParser.json())
@@ -39,4 +37,23 @@ mongoose.connect(
     // .catch is a method to catch any errors that might happen in a promise
 }).catch((err) => {
     console.log(`DB connection error ${err.message}`)
+})
+
+
+
+// =================================
+//            GET METHOD
+// =================================
+
+// here we are setting up the /allProduct route
+app.get('/allProduct', (req, res) => {
+    // .then is method in which we can chain functions on
+    // chaining means that once something has run, then we can
+    // run another thing
+    // the result variable is being returned by the .find() then we ran earlier
+    Product.find().then(result => {
+        // send back the result of the search to whoever asked for it
+        // send back the result to the front end. I.E the go the button
+        res.send(result)
+    })
 })
